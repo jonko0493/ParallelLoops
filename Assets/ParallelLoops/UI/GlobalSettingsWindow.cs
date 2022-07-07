c0c0b08127c16706c62a014ee4bed0e30d1fd9d2
jonko0493 <jonko0493@protonmail.com>
2022-07-06 17:54:29 -0700

    Add UXML for settings + buttons

--- Assets/ParallelLoops/UI/GlobalSettingsWindow.cs
+++ Assets/ParallelLoops/UI/GlobalSettingsWindow.cs
@@ -5,14 +5,14 @@ using UnityEngine.UIElements;
 
 public class GlobalSettingsWindow : EditorWindow
 {
-    //[SerializeField]
-    //private VisualTreeAsset m_VisualTreeAsset = default;
+    [SerializeField]
+    private VisualTreeAsset m_VisualTreeAsset = default;
 
     [MenuItem("Parallel Loops/Settings")]
     public static void ShowExample()
     {
         GlobalSettingsWindow wnd = GetWindow<GlobalSettingsWindow>();
-        wnd.titleContent = new GUIContent("GlobalSettingsWindow");
+        wnd.titleContent = new GUIContent("Settings");
     }
 
     private static TextField m_HeiretsuDirectoryPath;
@@ -20,29 +20,53 @@ public class GlobalSettingsWindow : EditorWindow
 
     public void OnEnable()
     {
-        m_HeiretsuDirectoryPath = new() { label = "Heiretsu Game Directory" };
-        m_BlenderPath = new() { label = "Blender Executable" };
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        //Instantiate UXML
+        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
+        root.Add(labelFromUXML);
+
+        m_HeiretsuDirectoryPath = labelFromUXML.Q<TextField>("heiretsuPathTextField");
+        m_BlenderPath = labelFromUXML.Q<TextField>("blenderPathTextField");
+
+        Button heiretsuPathButton = labelFromUXML.Q<Button>("heiretsuPathButton");
+        Button blenderPathButton = labelFromUXML.Q<Button>("blenderPathButton");
+        Button saveSettingsButton = labelFromUXML.Q<Button>("saveSettingsButton");
+        heiretsuPathButton.clicked += HeiretsuPathButton_clicked;
+        blenderPathButton.clicked += BlenderPathButton_clicked;
+        saveSettingsButton.clicked += SaveSettingsButton_clicked;
+    }
+
+    private void HeiretsuPathButton_clicked()
+    {
+        string heiretsuPath = EditorUtility.OpenFolderPanel("Open unpacked Heiretsu files folder", "", "");
+        if (!string.IsNullOrEmpty(heiretsuPath))
+        {
+            m_HeiretsuDirectoryPath.value = heiretsuPath;
+        }
+    }
+
+    private void BlenderPathButton_clicked()
+    {
+        string blenderPath = EditorUtility.OpenFilePanel("Open unpacked Heiretsu files folder", "", "");
+        if (!string.IsNullOrEmpty(blenderPath))
+        {
+            m_BlenderPath.value = blenderPath;
+        }
+    }
+
+    private void SaveSettingsButton_clicked()
+    {
+        DestroyImmediate(this);
     }
 
     public void CreateGUI()
     {
         GlobalSettings.LoadSettings();
 
-        // Each editor window contains a root VisualElement object
-        VisualElement root = rootVisualElement;
-
-        Label label = new() { text = "Paths" };
-
         m_HeiretsuDirectoryPath.value = GlobalSettings.HeiretsuPath;
         m_BlenderPath.value = GlobalSettings.BlenderPath;
-
-        root.Add(label);
-        root.Add(m_HeiretsuDirectoryPath);
-        root.Add(m_BlenderPath);
-
-        // Instantiate UXML
-        //VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
-        //root.Add(labelFromUXML);
     }
 
     public void OnDestroy()
